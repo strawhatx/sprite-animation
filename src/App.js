125e0978ce1ee52155ea66b0ae76ecf2c141ac98
Nathaniel James <nathanieltjames24@gmail.com>
2023-01-05 01:17:22 -0500

    setting up for the sprte to get ready for the animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React, { useRef } from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [context, setContext] = useState(null);
+  const [img, setImg] = useState(null);
+  const [width, setWidth] = useState("600px");
+  const [height, setHeight] = useState("600px");
+
   const ref = useRef(null);
+
+  const getPlayer = () => {
+    const img = new Image();
+    img.src = "images/shadow_dog.png";
+    setImg(img);
+  };
+
+  const setDimensions = () => {
+    setWidth((ref.current.width = 600));
+    setHeight((ref.current.height = 600));
+  };
+
+  useEffect(() => {
+    setContext(ref.current.getContext("2d"));
+
+    setDimensions();
+    getPlayer();
+  }, [ref]);
 
   return (
     <div className="app">
-      <div className="canvas" ref={ref}></div>
+      <canvas className="canvas" ref={ref}></canvas>
     </div>
   );
 };
