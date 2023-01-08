e458a066660fbf97655c48742a02650eebd1e205
Nathaniel James <nathanieltjames24@gmail.com>
2023-01-07 22:19:09 -0500

    lets sop here for now

--- src/script.js
+++ src/script.js
@@ -2,6 +2,12 @@ const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 const width = (canvas.width = 600);
 const height = (canvas.height = 600);
+const spriteW = 575;
+const spriteH = 523;
+
+let x = 0;
+let y = 1;
+
 const player = new Image();
 
 player.src = "images/shadow_dog.png";
@@ -17,7 +23,17 @@ function animate() {
   //context.drawImage(image, sx, sy, sw, sh,dx, dy, dw, dh);
 
   //add player to area
-  context.drawImage(player, 0, 0, 200, 200, 0, 0, width, height);
+  context.drawImage(
+    player,
+    x * spriteW,
+    y * spriteH,
+    spriteW,
+    spriteH,
+    0,
+    0,
+    spriteW,
+    spriteH
+  );
 
   requestAnimationFrame(animate);
 }
